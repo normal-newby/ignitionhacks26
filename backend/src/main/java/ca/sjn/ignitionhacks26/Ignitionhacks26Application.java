94eb4153d7770431477b6a0a27710116ca18c66a
Nimo Liu <nimo.r.liu@gmail.com>
2026-08-22 07:19:45 +0800

    initialize

---
+++ backend/src/main/java/ca/sjn/ignitionhacks26/Ignitionhacks26Application.java
@@ -0,0 +1,13 @@
+package ca.sjn.ignitionhacks26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ignitionhacks26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ignitionhacks26Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ca/sjn/ignitionhacks26/Ignitionhacks26ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sjn.ignitionhacks26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ignitionhacks26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
